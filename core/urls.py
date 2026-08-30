07374b6fcdba737895714a815a4f452a0a1feb2c
Rafael-Tunes-Mathiensen <rafaelmathiensen@gmail.com>
2026-08-30 13:40:00 -0300

    feat: adicionar redirecionamento de páginas em estrutura de login

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from core.views import index
+from core.views import index, lista_tarefas
 
 urlpatterns = [
     path('', index, name='index'),
+    path('tarefas', lista_tarefas, name='tarefas'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def lista_tarefas(request):
+    return render(request, 'core/tarefas.html')
