39ba32504753936dc16adefdb47d59bca92b4b83
Rafael-Tunes-Mathiensen <rafaelmathiensen@gmail.com>
2026-08-30 15:42:05 -0300

    feat: adicionar dados reais a tabela da página de tarefas

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from core.models import Tarefa
 
 def index(request):
     return render(request, 'core/index.html')
 
 def lista_tarefas(request):
-    return render(request, 'core/tarefas.html')
+    lista = Tarefa.objects.all()
+    return render(request, 'core/tarefas.html', {'tarefas': lista})
