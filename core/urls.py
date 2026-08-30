f4fcf38d5bd01426f3a127505a9f06f27cf1e0d5
Rafael-Tunes-Mathiensen <rafaelmathiensen@gmail.com>
2026-08-30 16:49:12 -0300

    feat: implementar exclusão ao CRUD

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from core.views import index, lista_tarefas
+from core.views import criar_tarefa, index, lista_tarefas, deletar_tarefa
 
 urlpatterns = [
     path('', index, name='index'),
-    path('tarefas', lista_tarefas, name='tarefas'),
+    path('tarefas/', lista_tarefas, name='tarefas'),
+    path('cadastro/', criar_tarefa, name='criar_tarefa'),
+    path('deletar/<int:id>/', deletar_tarefa, name='deletar_tarefa')
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from core.models import Tarefa
 
 def index(request):
@@ -7,3 +7,20 @@ def index(request):
 def lista_tarefas(request):
     lista = Tarefa.objects.all()
     return render(request, 'core/tarefas.html', {'tarefas': lista})
+
+def criar_tarefa(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data')
+        status = request.POST.get('status') == 'on'
+        Tarefa.objects.create(titulo=titulo, descricao=descricao, data=data, status=status)
+        return redirect('tarefas')
+    return render(request, 'core/criar_tarefa.html')
+
+def deletar_tarefa(request, id):
+    tarefa = Tarefa.objects.get(id=id)
+    if request.method == 'POST':
+        tarefa.delete()
+        return redirect('tarefas')
+    return render(request, 'core/deletar_tarefa.html', {'tarefa': tarefa})
