605d3db4985ba1c5894dc711e6ef48ee1f3f976b
Rafael-Tunes-Mathiensen <rafaelmathiensen@gmail.com>
2026-08-30 15:10:08 -0300

    feat: criar banco de dados e aplicar as migrações

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=50, blank=False, null=False)
+    descricao = models.CharField(max_length=500, blank=False, null=False)
+    data = models.DateField(null=False)
+    status = models.BooleanField(null=False)
+
+    def __str__(self):
+        return self.titulo
